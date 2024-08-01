e2ce294e7c03ed9157d129b52bf16db716b1671a
abdo0-0ayman <abdoayman9@gmail.com>
2024-08-01 20:45:55 +0300

    add student3

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let arr = [] ; 
+let arr = [];
 
 
 arr.push({
-    name : "mohamed elaraby" , 
-    email: "mohamed@gmail.com" , 
-    age  : 21 ,
+    name: "mohamed elaraby",
+    email: "mohamed@gmail.com",
+    age: 21,
 
-    getData : function(){
-        console.log(this.name , this.email , this.age) ; 
+    getData: function () {
+        console.log(this.name, this.email, this.age);
     }
 })
 
@@ -16,16 +16,30 @@ arr.push({
     email: "abdelrhmanhany2003@gmail.com",
     age: 21,
 
-    getData : function(){
-        console.log(this.name,this.email,this.age);
+    getData: function () {
+        console.log(this.name, this.email, this.age);
     }
 
 })
 
-arr[0].getData() ; 
-let student1 = JSON.stringify(arr[0]) ; 
-window.localStorage.user1 = student1 ; 
+arr.push({
+    name: "abdelrahman ayman",
+    email: "abdelrahmanayman@gmail.com",
+    age: 21,
+    getData: function () {
+        console.log(this.name, this.email, this.age);
+    }
+
+})
+
+arr[0].getData();
+let student1 = JSON.stringify(arr[0]);
+window.localStorage.user1 = student1;
 
 arr[1].getData();
 let student2 = JSON.stringify(arr[1]);
 window.localStorage.user2 = student2;
+
+arr[2].getData();
+let student3 = JSON.stringify(arr[2]);
+window.localStorage.user3 = student3;
