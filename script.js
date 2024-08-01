90855c3fb8b7fd09a11b2d49dc07f56610da0810
Abdo-hany48 <abdelrhmanhany200348@gmail.com>
2024-08-01 18:44:45 +0300

    adding second object and saving the object in local storage

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ arr.push({
     }
 })
 
+arr.push({
+    name: "abdelrhman hany",
+    email: "abdelrhmanhany2003@gmail.com",
+    age: 21,
+
+    getData : function(){
+        console.log(this.name,this.email,this.age);
+    }
+
+})
+
 arr[0].getData() ; 
 let student1 = JSON.stringify(arr[0]) ; 
 window.localStorage.user1 = student1 ; 
+
+arr[1].getData();
+let student2 = JSON.stringify(arr[1]);
+window.localStorage.user2 = student2;
